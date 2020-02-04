e389e52e256534fcf66a04ba90637f5eab71ee10
Andrew Huynh <a5thuynh@gmail.com>
2020-02-04 15:07:57 -0800

    Add beanstalk CLI path & fish color defaults

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -30,6 +30,10 @@ if not contains "/usr/local/opt/gettext/bin" $fish_user_paths
     set -U fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths;
 end
 
+# Add path for elastic beanstalk CLI
+if not contains "$HOME/.ebcli-virtual-env/executables" $fish_user_paths
+    set -U fish_user_paths "$HOME/.ebcli-virtual-env/executables" $fish_user_paths;
+end
 
 if not contains "$HOME/.poetry/bin" $fish_user_paths
     set -U fish_user_paths "$HOME/.poetry/bin" $fish_user_paths
@@ -50,3 +54,27 @@ if not functions -q fisher
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+# Colors:
+set fish_color_normal F8F8F2 # the default color
+set fish_color_command F92672 # the color for commands
+set fish_color_quote E6DB74 # the color for quoted blocks of text
+set fish_color_redirection AE81FF # the color for IO redirections
+set fish_color_end F8F8F2 # the color for process separators like ';' and '&'
+set fish_color_error F8F8F2 --background=F92672 # the color used to highlight potential errors
+set fish_color_param A6E22E # the color for regular command parameters
+set fish_color_comment 75715E # the color used for code comments
+set fish_color_match F8F8F2 # the color used to highlight matching parenthesis
+set fish_color_search_match --background=49483E # the color used to highlight history search matches
+set fish_color_operator AE81FF # the color for parameter expansion operators like '*' and '~'
+set fish_color_escape 66D9EF # the color used to highlight character escapes like '\n' and '\x70'
+set fish_color_cwd 66D9EF # the color used for the current working directory in the default prompt
+
+# Additionally, the following variables are available to change the highlighting in the completion pager:
+set fish_pager_color_prefix F8F8F2 # the color of the prefix string, i.e. the string that is to be completed
+set fish_pager_color_completion 75715E # the color of the completion itself
+set fish_pager_color_description 49483E # the color of the completion description
+set fish_pager_color_progress F8F8F2 # the color of the progress bar at the bottom left corner
+set fish_pager_color_secondary F8F8F2 # the background color of the every second completion
+# tabtab source for packages
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
