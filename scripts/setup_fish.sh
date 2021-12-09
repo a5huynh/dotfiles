ec1644a675575fc756a078d1ac41f29ac691b8da
Andrew Huynh <a5.t.huynh@gmail.com>
2021-12-08 16:40:25 -0800

    🐟 Check for existing fisher installation, smarter file copies

--- scripts/setup_fish.sh
+++ scripts/setup_fish.sh
@@ -12,11 +12,18 @@ else
 fi
 
 # Installs/updates fisher, fish plugin manager
-fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher";
+if [ -d "$HOME/.config/fish/functions/fisher.fish" ]; then
+    echo "-> installing fisher, fish plugin manager";
+    fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher";
+else
+    echo "-> fish plugin manager already installed, skipping";
+fi
 
 # Copies fish_plugins file to config folder and installs fish plugins
-cp $PWD/.config/fish/fish_plugins $HOME/.config/fish;
+echo "-> install/updating fish plugins";
+cp -n $PWD/.config/fish/fish_plugins $HOME/.config/fish || true;
 fish -c "fisher update";
 
-# Copies over custom functions
-cp $PWD/.config/fish/functions/*.fish $HOME/.config/fish/functions
+# Copies over custom functions but doesn't overwrite existing ones.
+echo "-> installing custom functions";
+cp -n $PWD/.config/fish/functions/*.fish $HOME/.config/fish/functions || true;
