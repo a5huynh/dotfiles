7d7576b48998a42e19797605c76bb1d9b909f654
Andrew Huynh <a5.t.huynh@gmail.com>
2021-12-08 16:33:21 -0800

    🐟 Setup + install fish plugin manager & plugins

--- scripts/setup_fish.sh
+++ scripts/setup_fish.sh
@@ -10,3 +10,13 @@ else
     echo "-> linking fish config folder";
     ln -s $PWD/.config/fish $HOME/.config;
 fi
+
+# Installs/updates fisher, fish plugin manager
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher";
+
+# Copies fish_plugins file to config folder and installs fish plugins
+cp $PWD/.config/fish/fish_plugins $HOME/.config/fish;
+fish -c "fisher update";
+
+# Copies over custom functions
+cp $PWD/.config/fish/functions/*.fish $HOME/.config/fish/functions
